f2849c40b20d2255d75eb680af420bccac2e1ccb
Bora <bora@borauzun.net>
2025-03-30 11:34:37 +0100

    divs - for course

--- app/(routes)/market/dividend/pageContent.tsx
+++ app/(routes)/market/dividend/pageContent.tsx
@@ -1,13 +1,33 @@
 'use client';
+import MainLayout from "@/app/components/layout/MainLayout";
+import PageContentContainer from "@/app/components/layout/PageContentContainer";
+import SidebarHome from "@/app/components/layout/sidebar/SidebarHome";
 import DividendSummaryGrid, { IGroupDivStats } from "@/app/components/ui/grids/DividendSummaryGrid";
 import { TickerDivData } from "@/app/interface/ITickerExtra";
+import Grid from "@mui/material/Grid2";
+
 interface IProps {
     results:IGroupDivStats[];
 }
 
 function PageContent(props:IProps) {
     return (  
-        <DividendSummaryGrid results={props.results} />
+
+<MainLayout inProgress={false}>
+     {/*  Sidebar */}
+     <Grid size={2}>
+         <SidebarHome />
+      </Grid>
+     {/*  Page Content */}
+     <Grid size={10}>
+        <PageContentContainer pageHeaderTitle="Dividend stats(All markets)" warnings={[]}>
+            {/* ROW 1 :Results */}
+           <DividendSummaryGrid results={props.results} />
+        </PageContentContainer>
+    </Grid>
+    
+</MainLayout>
+        
     );
 }
 
