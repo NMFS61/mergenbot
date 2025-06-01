809b47f81a0562ab88cfc21035544656cadb33e5
Bora Uzun <borauzun@Boras-MacBook-Pro.local>
2025-06-01 09:23:52 +0100

    flow

--- app/(routes)/flow/_ui/ParallelagramNode.tsx
+++ app/(routes)/flow/_ui/ParallelagramNode.tsx
@@ -1,11 +1,12 @@
 import { Box } from '@mui/material';
 import { Handle, Position } from '@xyflow/react';
-
+import MarketGroupSelection from "@/app/components/layout/sidebar/MarketChoiceSelection";
 function ParallelagramNode() {
     return ( 
     <>
-    <Box sx={{ width: '100px', height: '50px', backgroundColor: '#AAA239', transform: 'skew(20deg)' }}>
+    <Box sx={{ width: '200px', height: '100px', backgroundColor: '#AAA239', transform: 'skew(20deg)' }}>
     <Handle type="source" position={Position.Bottom} className='custom-handle' />
+    <MarketGroupSelection marketChoiceChanged={()=>{}}/>
     <Handle type="target" position={Position.Top} className='custom-handle' />
     </Box>
     </> );
--- app/(routes)/flow/_ui/RectangleNode.tsx
+++ app/(routes)/flow/_ui/RectangleNode.tsx
@@ -1,12 +1,15 @@
 
+import DateIntervalSelection from '@/app/components/layout/sidebar/DateIntervalSelection';
 import { Box } from '@mui/material';
 import { Handle, Position } from '@xyflow/react';
 function RectangleNode() {
     return ( 
     <>
-    <Box sx={{ width: '100px', height: '50px', backgroundColor: '#AAA239', borderRadius: '5px' }}>
-    <Handle type="source" position={Position.Bottom} className='custom-handle' />
+    <Box sx={{ width: '200px', height: '10px', borderRadius: '5px' }}>
+    
     <Handle type="target" position={Position.Top} className='custom-handle' />
+    <DateIntervalSelection onDatesDataChange={(dateData)=>{}}  />
+        <Handle type="source" position={Position.Bottom} className='custom-handle' />
     </Box>
     </> );
 }
